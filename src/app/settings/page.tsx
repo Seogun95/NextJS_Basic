31135e7b63c293a146efce852cd90d283ff24f59
Seogun95 <ksj083895@gmail.com>
2023-07-04 02:14:40 +0900

    step_10: ISR 렌더링

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,18 @@
 import { getSettingMenus } from '@/service/settingMenu';
+import axios from 'axios';
 import Link from 'next/link';
+import styles from '@/app/settings/page.module.css';
 
-export const revalidate = 3; // revalidate this page every 3 seconds
+// export const revalidate = 3;
 
 export default async function SettingHome() {
   const settings = await getSettingMenus();
+
+  const response = await fetch('https://meowfacts.herokuapp.com', {
+    cache: 'no-store',
+  })
+    .then(res => res.json())
+    .then(({ data }) => data[0]);
 
   return (
     <>
@@ -16,6 +24,8 @@ export default async function SettingHome() {
           </li>
         ))}
       </ul>
+      <hr />
+      <article className={styles.article}>{response}</article>
     </>
   );
 }
